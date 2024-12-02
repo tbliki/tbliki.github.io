6135077eb1b1f1aac4fb7e93fd5446f75d392fda
Timofej Bliki <timofej.bliki@ru.nl>
2024-12-02 19:58:28 +0100

    Added some styling to "Hello World". - Created a tiny JS function to dynamically change the text on-click and on-load to different ways of printing hello world

--- js/index.js
+++ js/index.js
@@ -6,8 +6,26 @@ const hellos = [
     "<h1>Hello, World!</h1>"
 ];
 
+let isTyping = false;
+
 function setHello() {
-    document.getElementById("hello-world").innerText = hellos[Math.floor(Math.random() * hellos.length)];
+    if (!isTyping) {
+        isTyping = true;
+
+        let prev = document.getElementById("hello-world").innerText;
+        let next = hellos[Math.floor(Math.random() * hellos.length)];
+        while (prev === next) {
+            next = hellos[Math.floor(Math.random() * hellos.length)];
+        }
+        let location = document.getElementById("hello-world");
+        for (let i = 0; i <= prev.length; i++) {
+            setTimeout(() => {location.innerText = prev.substring(0, prev.length - i);}, 750 + i * 40);
+        }
+        for (let i = 1; i <= next.length; i++) {
+            setTimeout(() => {location.innerText = next.substring(0, i);}, 1500 + (i + prev.length) * 20);
+        }
+        setTimeout(() => {isTyping = false;}, 1500 + (next.length + prev.length) * 20);
+    }
 }
 
 window.onload = setHello;
